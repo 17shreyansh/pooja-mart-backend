438572b505d5de64a649659af4da6eef772a93af
Shreyansh <kake0shreyansh@gmail.com>
2025-09-19 20:00:02 +0530

    link

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(helmet({
   crossOriginEmbedderPolicy: false
 }));
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 'your-frontend-domain.com' : 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production' ? 'https://pooja-mart.vercel.app' : 'http://localhost:5173',
   credentials: true
 }));
 
